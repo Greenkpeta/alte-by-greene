ca74b1e41f089bc31d887d6605d5e21a05793626
Amarachukwu Onuoha <amarachukwuonuohabars007@gmail.com>
2025-04-27 11:47:56 +0100

    dev server

---
+++ server.js
@@ -0,0 +1,19 @@
+//this can be removed on deployment.. i only created this to test on multiple devices
+
+import express from 'express';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const app = express();
+const port = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'homepage.html'))
+})
+app.listen(port, ()=>{
+    console.log("server running in http://localhost:3000")
+})
